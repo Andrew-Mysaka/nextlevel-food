73a8a1070ac7239921a70bcd2fb1c6083d5ffa8f
Andrew-Mysaka <andrewmysaka@gmail.com>
2024-01-02 22:08:12 +0200

    Using Client Components Efficiently

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "@/components/main-header/main-header-background";
+import NavLink from "@/components/main-header/nav-link";
 
 export default function MainHeader() {
     return (
@@ -14,11 +15,14 @@ export default function MainHeader() {
                     <Image src={logoImg} alt={"A plate with food on it"} priority/>
                     NextLevel Food
                 </Link>
-
                 <nav className={classes.nav}>
                     <ul>
-                        <li><Link href={"/meals"}>Browse Meals</Link></li>
-                        <li><Link href={"/community"}>Foodies Community</Link></li>
+                        <li>
+                            <NavLink href={"/meals"}>Browse Meals</NavLink>
+                        </li>
+                        <li>
+                            <NavLink href={"/community"}>Foodies Community</NavLink>
+                        </li>
                     </ul>
                 </nav>
             </header>
