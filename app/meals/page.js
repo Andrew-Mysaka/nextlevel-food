b08dcc70ddddb0287e3a54b8873aeb435262c897
Andrew-Mysaka <andrewmysaka@gmail.com>
2024-01-05 21:35:38 +0200

    Fetching Data By Leveraging NextJS & Fullstack Capabilities

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,10 @@
 import classes from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
+import {getMeals} from "@/lib/meals";
 
-function MealsPage() {
-
+async function MealsPage() {
+    const meals = await getMeals();
 
     return (
         <>
@@ -17,7 +18,7 @@ function MealsPage() {
                 </p>
             </header>
             <main>
-                <MealsGrid meals={[]}/>
+                <MealsGrid meals={meals}/>
             </main>
         </>
     );
